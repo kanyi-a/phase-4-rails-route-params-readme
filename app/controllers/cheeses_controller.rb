391fe0f509fca8a85cce90a6e8540b7fe8c00a45
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-09-23 21:59:54 +0300

    using params

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,11 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
-
+  def show
+    #find a cheese using ID from the url
+    #send a json esponse using that cheese object
+    #byebug
+    cheese = Cheese.find_by(id: params[:id])
+    render json:  cheese
+  end
 end
